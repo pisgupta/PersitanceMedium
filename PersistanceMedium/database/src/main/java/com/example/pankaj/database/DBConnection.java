c337a45fcb5f4ad65541c4f43835b05f6799656b
pisgupta@gmail.com <rap.is.gupta@gmail.com>
2015-08-06 13:10:51 +0530

    Update Method
    Update Method

--- PersistanceMedium/database/src/main/java/com/example/pankaj/database/DBConnection.java
+++ PersistanceMedium/database/src/main/java/com/example/pankaj/database/DBConnection.java
@@ -2,8 +2,11 @@ package com.example.pankaj.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Pankaj on 8/5/2015.
@@ -16,22 +19,54 @@ public class DBConnection {
         try {
             myDataBase = new MyDataBase(ct);
             sqLiteDatabase = myDataBase.getWritableDatabase();
-            Log.e("Table and db","Both created");
+            Log.e("Table and db", "Both created");
         } catch (Exception ex) {
-            Log.e("Table and db",ex.toString());
+            Log.e("Table and db", ex.toString());
         }
     }
-    public boolean insertData(EmployeeBean bean){
+
+    public boolean insertData(EmployeeBean bean) {
         boolean flag = false;
         ContentValues cv = new ContentValues();
-        cv.put(MyDataBase.TCNAME,bean.getId());
-        cv.put(MyDataBase.TCADDRESS,bean.getAddress());
-        cv.put(MyDataBase.TCAGE,bean.getAge());
-        long rowaffcted = sqLiteDatabase.insert(MyDataBase.TABLENAME,null,cv);
-        if(rowaffcted>0){
+        cv.put(MyDataBase.TCNAME, bean.getId());
+        cv.put(MyDataBase.TCADDRESS, bean.getAddress());
+        cv.put(MyDataBase.TCAGE, bean.getAge());
+        long rowaffcted = sqLiteDatabase.insert(MyDataBase.TABLENAME, null, cv);
+        if (rowaffcted > 0) {
             flag = true;
         }
-        return  flag;
+        return flag;
+    }
+
+    String[] allcolumnname = {MyDataBase.TCID, MyDataBase.TCNAME, MyDataBase.TCADDRESS, MyDataBase.TCAGE};
+
+    public ArrayList<String> selectAllData() {
+        ArrayList<String> list = new ArrayList<String>();
+        Cursor c = sqLiteDatabase.query(MyDataBase.TABLENAME, allcolumnname, null, null, null, null, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            int id = c.getInt(0);
+            String name = c.getString(1);
+            String add = c.getString(2);
+            int age = c.getInt(3);
+            list.add(id + " " + name + " " + add + " " + age);
+            c.moveToNext();
+        }
+        return list;
+    }
+
+    public boolean upTableData(EmployeeBean bean, int empid) {
+        boolean flag = false;
+        ContentValues cv = new ContentValues();
+      //  cv.put(MyDataBase.TCNAME, bean.getId());
+        cv.put(MyDataBase.TCADDRESS, bean.getAddress());
+        cv.put(MyDataBase.TCAGE, bean.getAge());
+
+       int row = sqLiteDatabase.update(MyDataBase.TABLENAME, cv,MyDataBase.TCID+" = ? ", new String[]{Integer.valueOf(empid).toString()});
+        if(row>0){
+            flag = true;
+        }
+        return flag;
     }
 }
 
--- PersistanceMedium/database/src/main/java/com/example/pankaj/database/MainActivity.java
+++ PersistanceMedium/database/src/main/java/com/example/pankaj/database/MainActivity.java
@@ -5,25 +5,32 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
-DBConnection con;
+    DBConnection con;
     EditText edtid, edtaddress, edtage;
     Button btnwrite, btnread;
+    ListView displaydata;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         con = new DBConnection(MainActivity.this);
 
-        edtid = (EditText)findViewById(R.id.edtid);
-        edtaddress = (EditText)findViewById(R.id.edtaddress);
-        edtage = (EditText)findViewById(R.id.edtage);
-        btnwrite =(Button)findViewById(R.id.btnsubmit);
+        edtid = (EditText) findViewById(R.id.edtid);
+        edtaddress = (EditText) findViewById(R.id.edtaddress);
+        edtage = (EditText) findViewById(R.id.edtage);
+        btnwrite = (Button) findViewById(R.id.btnsubmit);
+        displaydata = (ListView) findViewById(R.id.displaydata);
         btnwrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,11 +39,33 @@ DBConnection con;
                 bean.setAddress(edtaddress.getText().toString());
                 bean.setAge(Integer.parseInt(edtage.getText().toString()));
 
-                if(con.insertData(bean)){
-                    Toast.makeText(MainActivity.this,"Save",Toast.LENGTH_SHORT).show();
+                if (con.insertData(bean)) {
+                    Toast.makeText(MainActivity.this, "Save", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Save", Toast.LENGTH_SHORT).show();
                 }
-                else{
-                    Toast.makeText(MainActivity.this,"Not Save",Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.btnselect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> data = con.selectAllData();
+                ArrayAdapter<String> ad = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+                displaydata.setAdapter(ad);
+            }
+        });
+        findViewById(R.id.btnupdate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EmployeeBean bean = new EmployeeBean();
+                //bean.setId(edtid.getText().toString());
+                bean.setAddress(edtaddress.getText().toString());
+                bean.setAge(Integer.parseInt(edtage.getText().toString()));
+                boolean flag = con.upTableData(bean, Integer.parseInt(edtid.getText().toString()));
+                if (flag) {
+                    Toast.makeText(MainActivity.this, "Data Update", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Data Not Update", Toast.LENGTH_SHORT).show();
                 }
             }
         });
